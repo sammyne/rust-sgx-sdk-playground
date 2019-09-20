9c72e1461aa74eb37f4f37e30396d03345caa6f5
Sammy <lixiangmin01@baidu.com>
2019-09-20 20:05:01 +0800

    feature(cmake): addon to 'make clean'

--- app/build.rs
+++ app/build.rs
@@ -36,7 +36,7 @@ fn main () {
                     .unwrap_or_else(|_| "HW".to_string());
 
     println!("cargo:rustc-link-search=native=../lib");
-    println!("cargo:rustc-link-lib=static=Enclave_u");
+    println!("cargo:rustc-link-lib=static=enclave_u");
 
     println!("cargo:rustc-link-search=native={}/lib64", sdk_dir);
     match is_sim.as_ref() {
